baa7a5c89597d5f6ef8ec557189521b3d7796ffa
Hyunjong Kim <hjkim0811@snu.ac.kr>
2022-05-28 20:51:32 +0900

    add caption extraction

---
+++ caption-extraction/caption_extraction.py
@@ -0,0 +1,66 @@
+from math import dist
+
+# https://stackoverflow.com/questions/4978323/how-to-calculate-distance-between-two-rectangles-context-a-game-in-lua
+# rotation은 고려 x
+def rect_distance(RectA, RectB):
+    
+    A_x0, A_y0, A_x1, A_y1 = RectA
+    B_x0, B_y0, B_x1, B_y1 = RectB
+    
+    # 변수명: A 기준
+    left = B_x1 < A_x0 
+    right = A_x1 < B_x0
+    bottom = B_y1 < A_y0
+    top = A_y1 < B_y0
+    
+    if top and left:
+        return dist((A_x0, A_y1), (B_x1, B_y0))
+    elif left and bottom:
+        return dist((A_x0, A_y0), (B_x1, B_y1))
+    elif bottom and right:
+        return dist((A_x1, A_y0), (B_x0, B_y1))
+    elif right and top:
+        return dist((A_x1, A_y1), (B_x0, B_y0))
+    elif left:
+        return A_x0 - B_x1
+    elif right:
+        return B_x0 - A_x1
+    elif bottom:
+        return A_y0 - B_y1
+    elif top:
+        return B_y0 - A_y1
+    else:             # rectangles intersect
+        return 0.0
+
+# y값의 차이만 계산 (수직 거리)
+# rect_distance에서 A와 B의 x0과 x1은 같은 경우
+def diff_height(RectA, RectB):
+
+    _, A_y0, _, A_y1 = RectA
+    _, B_y0, _, B_y1 = RectB
+
+    # 변수명: A 기준
+    bottom = B_y1 < A_y0
+    top = A_y1 < B_y0
+
+    if bottom:
+        return A_y0 - B_y1
+    elif top:
+        return B_y0 - A_y1
+    else:
+        return 0.0
+
+# RectA가 RectB를 포함하는지 체크
+def contains(RectA, RectB):
+
+    A_x0, A_y0, A_x1, A_y1 = RectA
+    B_x0, B_y0, B_x1, B_y1 = RectB
+
+    return A_x0 < B_x0 < B_x1 < A_x1 and A_y0 < B_y0 < B_y1 < A_y1
+
+# image, text에서 사용
+def get_bbox(obj):
+    bbox = (round(obj['x0'], 2), round(obj['top'], 2), round(obj['x1'], 2), round(obj['bottom'], 2))
+    return bbox
+
+
